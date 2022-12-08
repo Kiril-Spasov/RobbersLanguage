81965b83d962694ca82cb00d1343b6dd6866c326
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-08 18:48:20 +0200

    Add project files.

---
+++ RobbersLanguage/FormRobbersLanguage.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RobbersLanguage
+{
+    partial class FormRobbersLanguage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Location = new System.Drawing.Point(48, 44);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(174, 63);
+            this.BtnConvert.TabIndex = 0;
+            this.BtnConvert.Text = "Convert To Robbers Language";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(248, 44);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(483, 361);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormRobbersLanguage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnConvert);
+            this.Name = "FormRobbersLanguage";
+            this.Text = "Robbers Language";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ RobbersLanguage/FormRobbersLanguage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RobbersLanguage
+{
+    public partial class FormRobbersLanguage : Form
+    {
+        public FormRobbersLanguage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\robbers.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    TxtResult.Text += ConvertToRobbersLanguage(line) + Environment.NewLine;
+                }
+            }
+        }
+
+        private string ConvertToRobbersLanguage(string text)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+            string vowels = "aeiou";
+            string result = "";
+
+            int closestBefore = 0;
+            int closestAfter = 0;
+            int distanceBefore = 0;
+            int distanceAfter = 0;
+
+            //Itterate through the entire string.
+            for (int i = 0; i < text.Length; i++)
+            {
+                //Pull appart each letter.
+                string letter = text.Substring(i, 1);
+
+                //Check if letter is a consonant.
+                if (consonants.IndexOf(letter) != -1)
+                {
+                    //Add the consonant to the string result.
+                    result += letter;
+
+                    //Itterate the alphabet from the possition of the letter towards the begining.
+                    for (int j = alphabet.IndexOf(letter); j >= 0 ; j--)
+                    {
+                        //Find the closest vowel and calculate the distance.
+                        if (vowels.IndexOf(alphabet.Substring(j, 1)) != -1)
+                        {
+                            closestBefore = j;
+                            distanceBefore = alphabet.IndexOf(letter) - j;
+                            break;
+                        }
+                    }
+
+                    //Itterate the alphabet the position of the letter till the end.
+                    for (int j = alphabet.IndexOf(letter); j < alphabet.Length ; j++)
+                    {
+                        //Find the closest vowel and calculate distance.
+                        if (vowels.IndexOf(alphabet.Substring(j, 1)) != -1)
+                        {
+                            closestAfter = j;
+                            distanceAfter = j - alphabet.IndexOf(letter);
+                            break;
+                        }
+                    }
+
+                    //Check which vowel has closer distance and add it to the result.
+                    if (distanceBefore < distanceAfter || distanceBefore == distanceAfter)
+                    {
+                        result += alphabet.Substring(closestBefore, 1);
+                    }
+                    else
+                    {
+                        result += alphabet.Substring(closestAfter, 1);
+                    }
+
+                    
+                    if (letter == "z")
+                    {
+                        result += letter;
+                    }
+                    else
+                    {
+                        //Itterate the alphabet from the position of the letter till the end and
+                        //find the next consonant.
+                        for (int j = alphabet.IndexOf(letter) + 1; j < alphabet.Length; j++)
+                        {
+                            if (consonants.IndexOf(alphabet.Substring(j, 1)) != -1)
+                            {
+                                result += alphabet.Substring(j, 1);
+                                break;
+                            }
+                        }
+                    }        
+                }
+                //If it's a vowel - add the letter to the result.
+                else
+                {
+                    result += letter;
+                }
+            }
+            return result;
+        }
+    }
+}
